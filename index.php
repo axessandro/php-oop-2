146004724d0b8b4f527ab793b373a7fdfc702d0d
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-12-12 16:24:16 +0100

    Already to fix

--- index.php
+++ index.php
@@ -5,11 +5,13 @@ require_once __DIR__ . "/Models/Category.php";
 require_once __DIR__ . "/Models/UnderCategory.php";
 
 foreach ($products as $productItem) {
-    $name = $productItem->name;
-    $category = $productItem->category;
-    $under_category = $productItem->under_category;
-    $size = $productItem->size;
-    $price = $productItem->price;
+    $name = $productItem["name"];
+    $category = $productItem["category"];
+    $under_category = $productItem["under_category"];
+    $size = $productItem["size"];
+    $price = $productItem["price"];
 
-    $item = new Product($name, $category, $under_category, $size, $price);
+    $new_category = new Category($category);
+    $new_under_category = new UnderCategory($under_category);
+    $new_product = new Product($name, $new_category, $new_under_category, $size, $price);
 }
